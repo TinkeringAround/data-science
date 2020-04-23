26ec46fe69512a9d75592c5ffd0a7f5e3be229da
Thomas Maier <thomas.maier@juniq-it.de>
2020-04-23 11:13:01 +0200

    Finish Game for ESA3

--- 3_pythonExcercise.py
+++ 3_pythonExcercise.py
@@ -1,26 +1,54 @@
 # Modules
+import numpy as np
 import random
-
-# Declarations
+import requests
+from os import system, name
 
 
 class HangmanGame:
+    # Example API for Testing Purpose, Did not want to build one myself so I used the first free I found
+    API_URL = "https://random-word-api.herokuapp.com/word?number=1&swear=0"
     gameIsBeaten = False
-    lives = 3
+    lives = 16
+    round = 0
     word = ""
     maskedWord = ""
-    enteredKeys = []
-    PREDEFINED_WORDS = ["Teleskopgabel", "Wochenplaner",
-                        "Elefantenrennen"]  # Add More Words for less Duplications
+    enteredKeys = np.array([])
+    PREDEFINED_WORDS = ["extraordinary", "communication",
+                        "membership", "psychologist"]  # Default Words when Example API
 
+    # Starting Game
     def start(self):
+        self.clearConsole()
         print("-> Welcome to a simple Hangman Game! <-")
-        self.word = self.getRandomPredefinedWord(self.PREDEFINED_WORDS)
-        self.maskedWord = self.generateMaskFromWord(self.word)
-        self.startGameLoop()
+        self.printNewLine()
+        self.word = self.getRandomWordFromApi()
 
+        if len(self.word) == 0:
+            self.word = self.getRandomPredefinedWord(self.PREDEFINED_WORDS)
+
+        self.maskedWord = self.generateMaskFromWord(self.word)
+        self.enterGameLoop()
+
+    # Print Functions
     def printNewLine(self):
         print("\n")
+
+    def clearConsole(self):
+        if name == 'nt':
+            _ = system('cls')
+        else:
+            _ = system('clear')
+
+    def getRandomWordFromApi(self):
+        try:
+            r = requests.get(self.API_URL)
+            if r.status_code == 200 and len(r.json()) > 0:
+                return str(r.json()[0])
+            else:
+                return ""
+        except:
+            return ""
 
     def getRandomPredefinedWord(self, words):
         if isinstance(words, list):
@@ -34,46 +62,115 @@ class HangmanGame:
             mask += "_"
         return mask
 
-    def startGameLoop(self):
+    def enterGameLoop(self):
         while self.gameIsBeaten or self.lives > 0:
+            self.round += 1
+            if self.round > 1:
+                self.clearConsole()
+
             self.showGameOutput()
             input = self.waitForInput()
+
+            # Enter '1' to QUIT the Game
             if self.isQuitGameKey(input):
-                self.showWordAndQuit()
                 break
-            else:
-                # TODO: Compare and Update Masked
-                print("TODO: Compare and Update")
+
+            # Enter whole Word
+            if len(input) > 1:
+                if self.compareWords(self.word, input):
+                    self.gameIsBeaten = True
+                    break
+                else:
+                    self.lives -= 2
+
+            # Enter single Character
+            lowerCaseInput = input.lower()
+            if len(lowerCaseInput) == 1 and not np.isin(lowerCaseInput, self.enteredKeys):
+                self.enteredKeys = np.append(self.enteredKeys, lowerCaseInput)
+
+                if self.wordContainsCharacter(self.word, lowerCaseInput):
+                    self.maskedWord = self.updateMaskedWord(
+                        self.word, self.maskedWord, lowerCaseInput)
+                    if self.compareWords(self.word, self.maskedWord):
+                        self.gameIsBeaten = True
+                        break
+                else:
+                    self.lives -= 1
+
+        # End Screen
+        self.showWordAndQuit()
 
     def showGameOutput(self):
+        print("########## ROUND " +
+              str(self.round) + " ##########")
+        print("# Lives:         " + str(self.lives))
+        print("# Entered Keys:  " +
+              self.concatArrayElementsToString(self.enteredKeys))  # TODO: Add Hangman Man
         self.printNewLine()
-        print("############## ROUND " +
-              str(len(self.enteredKeys) + 1) + " ##############")
-        print("# Lives: " + str(self.lives))
+        print(self.maskedWord)
         self.printNewLine()
-        print("Hangman Word and Characters already entered: " + self.maskedWord +
-              " " + self.concatArrayElementsToString(self.enteredKeys))
 
     def isQuitGameKey(self, character):
         if character == "1":
             return True
 
     def showWordAndQuit(self):
-        print("The Word was: " + self.word)
+        self.clearConsole()
+        if self.gameIsBeaten:
+            print("########## YOU WON! ##########")
+        else:
+            print("########## GAME OVER ##########")
+
+        print("The Word to solve: " + self.word)
         print("You entered the following Keys: " +
               self.concatArrayElementsToString(self.enteredKeys))
         print("Thanks for playing this simple Hangman Game!")
+        self.printNewLine()
 
     def waitForInput(self):
-        return input("Type Character or '1' to QUIT: ")
+        return input("Type in a Character or '1' to QUIT: ")
 
     def concatArrayElementsToString(self, array):
         result = ""
         for element in array:
-            result += element + ";"
+            result += element + "; "
         return result
 
+    def wordContainsCharacter(self, word, character):
+        index = word.find(character)
+        return index >= 0
 
-# Initialization
-hangmanGame = HangmanGame()
-hangmanGame.start()
+    def updateMaskedWord(self, originalWord, maskedWord, character):
+        newMaskedWord = list(maskedWord)
+        indexList = self.getAllCharacterIndexInWord(originalWord, character)
+
+        for index in indexList:
+            newMaskedWord[index] = character
+
+        return "".join(newMaskedWord)
+
+    def getAllCharacterIndexInWord(self, word, character):
+        indexList = np.int_([])
+        wordLength = len(word)
+        index = 0
+
+        while index < wordLength:
+            if word[index] == character:
+                indexList = np.append(indexList, int(index))
+            index += 1
+
+        return indexList
+
+    def compareWords(self, word1, word2):
+        return word1.lower() == word2.lower()
+
+
+# Initialization & Game Loop
+playGame = True
+while playGame:
+    hangmanGame = HangmanGame()
+    hangmanGame.start()
+
+    inputKey = input("Press 1 to RESTART or Any Other Key to QUIT: ")
+    if not inputKey == "1":
+        playGame = False
